944765b9fcc0c4481f6cbf183917cc942f27e4a3
seo0033 <tkrhk9511@gmail.com>
2022-08-22 12:49:31 +0900

    20220822

--- react_basic00/src/App.js
+++ react_basic00/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import './basic.css';
 
-function App() {
+const App = () => {
+
+  const [num, setNum] = useState(1);
+  const link = useRef(1);
+  const [toggle, setToggle] = useState(true);
+
+  const Test = () => {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+  // console.log(num)
+  // console.log('App')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {num} / {link.current}
+
+      <section className={toggle ? '' : 'on'}>
+        <div>
+          {toggle ? 'loading...' : <Test />}
+        </div>
+      </section>
+
+      <div>000</div>
+      {/* {console.log(num, link.current)} */}
+      <button onClick={
+        () => (
+          setNum(num + 1),
+          console.log(toggle),
+          setToggle(!toggle),
+          console.log(num),
+          link.current = link.current + 1,
+          console.log(link.current)
+          // 화살표함수 → 여러개실행할때 ',' 사용
+        )
+      }>click</button>
+    </>
+  )
 }
 
 export default App;
+
+// 1. 일반함수 사용
+// function App() {
+//   return <div>001</div>
+//   }
+
+// 2. const 사용법(변수에 함수 담아쓰기)
+// const App = function(){
+//   return <div>001</div>
+// }
+
+// 3. const + 화살표함수 사용법
+// const App = () => {
+//   return <div>001</div>
+// }
